9fa9b27fe50ee2d941b6acc882f0ec929427b7c6
Kellen Presta <kpresta04@gmail.com>
2020-10-19 09:49:46 -0500

    overlap issue fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,60 @@
 <template>
-  <nuxt />
+  <v-app>
+    <v-app-bar
+      class="appBar"
+      absolute
+      color="#fcb69f"
+      dark
+      shrink-on-scroll
+      src="/ar1.jpg"
+      scroll-target="#scrolling-techniques-2"
+      app
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+      <v-toolbar-title>
+        <nuxt-link to="/">
+          Recipe DB
+        </nuxt-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn absolute bottom right nuxt>
+        <nuxt-link to="/login">Login</nuxt-link>
+      </v-btn>
+
+      <!-- <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn> -->
+
+      <!-- <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu> -->
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -29,3 +84,9 @@ export default {
   }
 };
 </script>
+<style scoped>
+a {
+  color: white;
+  text-decoration: none;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from "vuetify/es5/util/colors";
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: "%s - recipe-storehouse",
-    title: "recipe-storehouse",
+    titleTemplate: "%s - RecipeDB",
+    title: "RecipeDB",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ["~/css/global.css"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -37,7 +37,7 @@ export default {
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,

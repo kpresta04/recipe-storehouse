eb3016ed31a1f841eb7c4f95898c87de50e1c28e
Kellen Presta <kpresta04@gmail.com>
2021-02-12 11:40:51 -0600

    appbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar class="appBar" color="teal" src="/ar1.jpg" absolute dark app>
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
-
+    <v-app-bar class="appBar" color="transparent" absolute light app>
       <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
       <v-app-bar-nav-icon
         v-if="windowWith < 1024"
